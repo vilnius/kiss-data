37acda8586511f4c5ef9b1d214d3cb0c2c484898
Lukasyo <lukas.kondrusevicius@gmail.com>
2017-01-24 11:04:45 +0200

    removal of unnecessary files

--- darzeliu_grupes.R
+++
@@ -1,5 +0,0 @@
-#kindergarten groups by age and number of children in them
-
-grupes <- count(lankantys_grupes,vars = c("Grupės.tipas", "Darželio.pavadinimas"))
-grupes <- grupes[grupes$Darželio.pavadinimas != "_Testinis darželis Vilnius1", ]
-
